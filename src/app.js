136f8d4f18e09b56c1af279dc3db24e656d99066
josMuiruri <josemuiruri51@gmail.com>
2026-03-27 22:07:59 +0300

    implement morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
+const productRouter = require('./routes/productRouter');
+
 const app = express();
 
 app.use(express.json());
+
+// middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/products', productRouter);
 
